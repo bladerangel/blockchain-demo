6bbbdc80ba191515a91b4e2286c2a1d4cf5381af
bladerangel <bladerangel10@gmail.com>
2019-10-25 16:35:58 -0300

    Refactoring block class tests

--- tests/unit/specs/Block.spec.ts
+++ tests/unit/specs/Block.spec.ts
@@ -2,24 +2,12 @@ import Block from '../../../src/Block';
 
 describe('Tests in block class', () => {
   test('Should instantiate a block by passing values ​​correctly to the constructor', () => {
-    const block = new Block(0, '', '');
+    const block = new Block(0, 0, '', '', '', 0);
     expect(block.index).toBe(0);
-    expect(block.nonce.toString()).toHaveLength(5);
+    expect(block.nonce).toBe(0);
     expect(block.previousHash).toBe('');
-    expect(block.hash.toString()).toHaveLength(64);
+    expect(block.hash).toBe('');
     expect(block.data).toBe('');
-    expect(block.timestamp).not.toBeNaN();
-  });
-
-  test('Should instantiate the genesis block correctly', () => {
-    const block = Block.genesis();
-    expect(block.index).toBe(0);
-    expect(block.nonce.toString()).toHaveLength(5);
-    expect(block.previousHash).toBe(
-      '0000000000000000000000000000000000000000000000000000000000000000',
-    );
-    expect(block.hash.toString()).toHaveLength(64);
-    expect(block.data).toBe('genesis block');
-    expect(block.timestamp).not.toBeNaN();
+    expect(block.timestamp).toBe(0);
   });
 });
