e701c671edbb075c4f3c9103b50a6b95d8e2e564
bladerangel <bladerangel10@gmail.com>
2019-10-25 15:22:57 -0300

    Refactoring string length check condition

--- tests/unit/specs/Block.spec.ts
+++ tests/unit/specs/Block.spec.ts
@@ -4,9 +4,9 @@ describe('Tests in block class', () => {
   test('Should instantiate a block by passing values ​​correctly to the constructor', () => {
     const block = new Block(0, '', '');
     expect(block.index).toBe(0);
-    expect(block.nonce.toString().length).toBe(5);
+    expect(block.nonce.toString()).toHaveLength(5);
     expect(block.previousHash).toBe('');
-    expect(block.hash.toString().length).toBe(64);
+    expect(block.hash.toString()).toHaveLength(64);
     expect(block.data).toBe('');
     expect(block.timestamp).not.toBeNaN();
   });
@@ -14,11 +14,11 @@ describe('Tests in block class', () => {
   test('Should instantiate the genesis block correctly', () => {
     const block = Block.genesis();
     expect(block.index).toBe(0);
-    expect(block.nonce.toString().length).toBe(5);
+    expect(block.nonce.toString()).toHaveLength(5);
     expect(block.previousHash).toBe(
       '0000000000000000000000000000000000000000000000000000000000000000',
     );
-    expect(block.hash.toString().length).toBe(64);
+    expect(block.hash.toString()).toHaveLength(64);
     expect(block.data).toBe('genesis block');
     expect(block.timestamp).not.toBeNaN();
   });
